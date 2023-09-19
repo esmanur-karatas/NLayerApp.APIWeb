1d59ba78fbee4e5f4ad647097653f865bb4207df
Esmanur Karatas <esmanurkaratas0@gmail.com>
2023-09-19 10:28:06 +0300

    Initial commit

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository <T> where T : class
+    {
+        Task <T> GetByIdAsync (int id); // ıd dön bana 
+        IQueryable<T> GetAll (Expression<Func <T, bool>> expression); // IQueryable dönünce veri tabanına direk gitmek bir tolist gibi metotlarla gider.
+        IQueryable <T> Where (Expression<Func <T, bool>> expression);
+        Task<bool> AnyAsync (Expression<Func <T, bool>> expression);
+        Task AddAsync (T entity);
+        Task AddRangeAsync (IEnumerable<T> entities);
+        void Update (T entity);
+        void Remove (T entity);
+        void RemoveRange (IEnumerable<T> entities);
+    }
+}
---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id); // ıd dön bana 
+        Task<IEnumerable<T>> GetAllAsync(); // IQueryable dönünce veri tabanına direk gitmek bir tolist gibi metotlarla gider.
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
